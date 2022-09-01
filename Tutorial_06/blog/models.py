0458bfeb8541096a6528eabb8fe98b8ea6876d3e
GustavoBonif <gubonic@gmail.com>
2022-08-31 22:39:58 -0300

    Fazendo tutorial 06

--- Tutorial_06/blog/models.py
+++ Tutorial_06/blog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -9,5 +7,6 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+
     def __str__(self):
         return self.title
